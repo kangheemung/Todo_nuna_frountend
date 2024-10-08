308eaf5099dd2927eec446cf58e8ac242e16d5c6
kurotamashi30 <kurotamashi30@gmail.com>
2024-10-08 22:17:12 +0900

    proxy setting

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,34 +1,34 @@
-import axios from "axios";
+import axios from 'axios';
 //PROXY추가 함으로써 https에서 호출해도에러 안나게 조율
 const api = axios.create({
-  baseURL: `${process.env.REACT_APP_BECKEND_PROXY}/api`,
-  headers: {
-    "Content-Type": "application/json",
-  },
+    baseURL: `${process.env.REACT_APP_BECKEND_PROXY}/api`,
+    headers: {
+        'Content-Type': 'application/json',
+    },
 });
 /**
  * console.log all requests and responses
  */
 api.interceptors.request.use(
-  (request) => {
-    console.log("Starting Request", request);
-    return request;
-  },
-  function (error) {
-    console.log("REQUEST ERROR", error);
-  }
+    (request) => {
+        console.log('Starting Request', request);
+        return request;
+    },
+    function (error) {
+        console.log('REQUEST ERROR', error);
+    }
 );
 
 api.interceptors.response.use(
-  (response) => {
-    console.log("Response:", response);
-    return response;
-  },
-  function (error) {
-    error = error.response.data;
-    console.log("RESPONSE ERROR", error);
-    return Promise.reject(error);
-  }
+    (response) => {
+        console.log('Response:', response);
+        return response;
+    },
+    function (error) {
+        error = error.response.data;
+        console.log('RESPONSE ERROR', error);
+        return Promise.reject(error);
+    }
 );
 
 export default api;
