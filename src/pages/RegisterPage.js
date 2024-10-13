a395e09cb7c7d5c2251dd111aafec87374b16ec3
kurotamashi30 <kurotamashi30@gmail.com>
2024-10-13 20:44:17 +0900

    test

--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -1,30 +1,53 @@
-import React from "react";
+import React,{useState} from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
-
+import '../App.css';
 const RegisterPage = () => {
+
+    const [name,setName]=useState('');
+    const [email,setEmail]=useState('');
+    const [password,setPassword]=useState('');
+    const [secPassword,setSecPassword]=useState('');
+    const [error,setError]=useState('');
+    const handleSubmit =(e)=>{
+        e.preventDefault();
+        try{
+            if (!name || !email || !password || !secPassword)  {
+                throw new Error('Please fill in all fields');
+            }else if(password !== secPassword){
+            //에러:페스워드가 일치하지 않는다
+            throw new Error("not match password!");
+        }
+    
+        ///api
+        }catch(error){
+            setError(error.message);
+        }
+
+    }
   return (
     <div className="display-center">
-      <Form className="login-box">
+        {error && <div className="red-error">{error}</div>}
+      <Form className="login-box" onSubmit={handleSubmit}>
         <h1>회원가입</h1>
         <Form.Group className="mb-3" controlId="formName">
           <Form.Label>Name</Form.Label>
-          <Form.Control type="string" placeholder="Name" />
+          <Form.Control type="string" placeholder="Name" onChange={(e)=>setName(e.target.value)}/>
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Control type="email" placeholder="Enter email"  onChange={(e)=>setEmail(e.target.value)}/>
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control type="password" placeholder="Password" onChange={(e)=>setPassword(e.target.value)}/>
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>re-enter the password</Form.Label>
-          <Form.Control type="password" placeholder="re-enter the password" />
+          <Form.Control type="password" placeholder="re-enter the password" onChange={(e)=>setSecPassword(e.target.value)}/>
         </Form.Group>
 
         <Button className="button-primary" type="submit">
