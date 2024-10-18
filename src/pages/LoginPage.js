2215e1cfa04bff9cda1f6f2a6690f96af6464678
kurotamashi30 <kurotamashi30@gmail.com>
2024-10-18 23:00:52 +0900

    navigate

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,6 +1,6 @@
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import React, { useState } from 'react';
 import api from '../utils/api';
 import './LoginPage.style.css';
@@ -8,25 +8,31 @@ const LoginPage = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [error, setError] = useState('');
+    const [user, setUser] = useState(null);
+    const navigate = useNavigate();
     const handleLogin = async (e) => {
         e.preventDefault();
         try {
             if (!email || !password) {
-                throw new Error('Email or password is missing');
+                throw new Error('Email and password are required');
             }
             const response = await api.post('/user/login', { email, password });
             console.log('Response:', response);
-            if (response.data.status === 'fail' && response.data.error === 'User not found') {
-                throw new Error('User not found. Please check your email and password.');
-            }
 
             if (response.status === 200) {
+                setUser(response.data.user);
+                sessionStorage.setItem('token', response.data.token);
+                api.defaults.headers['authorization'] = 'Bearer ' + response.data.token;
+                setError('');
+                navigate('/');
                 // Successful response handling logic can be added here if needed
+            } else if (response.data.status === 'fail') {
+                throw new Error(response.data.error); // Throw an error if authentication fails
             } else {
-                throw new Error(response.data.error); // Adjusted to access the 'error' field for the message
+                throw new Error('An error occurred during login');
             }
         } catch (error) {
-             console.error(error); // Log the error message for debugging
+            console.error(error); // Log the error message for debugging
             setError(error.message);
         }
     };
